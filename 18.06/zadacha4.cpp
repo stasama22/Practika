11d1b1398b408da914a2c91e8a1d75b80857696f
stasama22 <stasama007@mail.ru>
2025-09-25 22:19:41 +0800

    l2

--- 18.06/zadacha4.cpp
+++ 18.06/zadacha4.cpp
@@ -11,22 +11,14 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> per[i];
     }
-
-    string str;
-    cin >> str;
-
-    string cur = str;
-    string t = str;
-
-    for (int i = 0; i < k; i++) {
-        t = cur;
-
+    string cur = "";
+    string f = "";
+    for (int i : k) {
+        f = cur;
         for (int j = 0; j < n; j++) {
-            int new_pos = per[j] - 1;
-            cur[new_pos] = t[j];
+            int ind = per[j] - 1;
+            cur[j] = f[ind];
         }
     }
-
     cout << cur << '\n';
-    return 0;
 }
